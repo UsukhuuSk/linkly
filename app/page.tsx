aef6edb3e5b19efd011a373d8582048145d9c5d2
Usukhbayar Tumen-Ulzii <usukhbayar.t@ema.gov.mn>
2025-03-19 11:06:09 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <main className="relative h-dvh overflow-x-hidden bg-slate-100"></main>
+  );
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+  return <div className="relative h-dvh overflow-x-hidden">Page</div>;
+}
