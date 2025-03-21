bfb6ef1e067106eda95c615bc2d6446a11eba4de
Usukhbayar Tumen-Ulzii <usukhbayar.t@ema.gov.mn>
2025-03-21 16:04:59 +0800

    update

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,11 +1,31 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
+import clsx from "clsx";
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4">
-      {/* AAAAAAAAAA */}
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <LinkScroll
         to="pricing"
         smooth
